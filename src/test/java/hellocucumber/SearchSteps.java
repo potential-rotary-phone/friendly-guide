f2573ad6c01b8d0d0f4cc69d9216d626fd51d531
Potential Rotary Phone <tranthanhtoan1957@outlook.com>
2024-07-27 11:57:11 +0000

    Update

--- src/test/java/hellocucumber/SearchSteps.java
+++ src/test/java/hellocucumber/SearchSteps.java
@@ -1,4 +1,4 @@
-package stepdefinitions;
+package hellocucumber;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/hellocucumber",
-        glue = "stepdefinitions",
+        glue = "hellocucumber",
         plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class TestRunner {
